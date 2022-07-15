48c2b51214346a680de84f13ecf2552d3d99d698
Hedi Kramer <hedi.louise.kramer@web.de>
2022-07-15 11:34:03 +0200

    largest

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,23 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "hedi";
-        } else { // TODO extend the programm here
+        } else if(query.contains("which of the following numbers is the largest:")) {
+            String[] split = query.split(":");
+            String numbers = split[1];
+            String[] numbersOnly = numbers.split(",");
+            int min = Integer.parseInt(numbersOnly[0]);
+            int index = 0;
+            for (int i = 0; i < numbersOnly.length; i++) {
+                if (Integer.parseInt(numbersOnly[index]) > min) {
+                    min = Integer.parseInt(numbersOnly[index]);
+                    index++;
+                }
+            }
+        }else if (query.contains("what is")) {
+            String[] split = query.split("\\s+");
+            int firstNumber = Integer.parseInt(split[2]) + Integer.parseInt(split[4]);
+            return String.valueOf(firstNumber);
+        }
             return "";
         }
-    }
 }
